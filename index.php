7075abb19eb1fdf7684a43fda39833a279e3b22b
natailton96 <33704071+natailton96@users.noreply.github.com>
2017-11-15 20:21:46 -0200

    html

---
+++ index.php
@@ -0,0 +1,513 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Documento sem título</title>
+		<link href="stylesheet.css" rel="stylesheet">
+	</head>
+	<body>
+
+	<div class="topo1"></div>
+	<div class="pulic">Publicidade</div>
+	
+	
+	<div class="topo2">
+		Ajuda &emsp; | &emsp; Contato
+	</div>
+	
+	
+	<div class="topo3">
+		<div class="pesquisa">
+			<img class="logo" src="image.jpg">
+			<form method="get">
+				<input type="text" name="pesquisar" id="text_pesquisa">
+				<select id="caixa_de_selecao">
+					<option>Categoria 1</option>
+					<option>Categoria 2</option>
+					<option>Categoria 3</option>
+					<option>-Categoria 4</option>
+					<option>-Categoria 5</option>
+					<option>Categoria 6</option>
+				</select>
+				<input class="bt_submit" type="submit" value="Pesquisar">
+			</form>
+		</div>
+	</div>
+
+	<div>
+		<table class="menu_topo" align="center">
+			<tr>
+				<td>CATEGORIAS</td>
+				<td>MAIS VENDIDOS</td>
+				<td>OFERTAS ESPECIAIS</td>
+				<td>TODAS AS LOJAS</td>
+				<td>VENDER</td>
+			</tr>
+		</table>			
+	</div>
+	
+	
+	<div class="menu_lateral">
+	
+		<table>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+			<tr>
+				<td class="menu_lateral_lista">Categoria Pai</td>
+				<td>></td>
+			</tr>
+		</table>
+	</div>
+	
+	<div>
+		<img class="banner" src="image.jpg">
+		<img class="banner_l" src="image.jpg">
+		<img class="banner_m" src="image.jpg">
+	</div>
+
+
+		<div class="area">Destaque</div>
+		
+		<div class="destaque">
+		
+			<div class="lateral_publicidade"></div>
+		
+		
+		
+		
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+					
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			</div>
+		
+		<div class="area">Ofertas</div>
+		
+		<div class="destaque">
+		
+			<div class="lateral_publicidade"></div>
+		
+		
+		
+		
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+					
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			<div class="produto">
+				<img src="image.jpg" class="img_produto">
+				
+				Descrição do produto, usando
+				até 2 linhas e até 70 caracteres
+			
+				<div class="preco">R$ 170.30 <div class="promocao">R$ 200.00</div></div>
+				<div class="estrela2"><img class="estrela" src="estrela.png"> <div class="estrala_numero">(325)</div></div>
+				<div class="barra_contagem">
+					<div class="barra_contagem_cheio">
+					</div>
+					<div class="barra_contagem_numero">300</div>
+				</div>
+			
+			</div>
+			
+			</div>
+		
+		<div class="area">Lojas em Destaque</div>
+		
+		<div class="div_loja_destaque">
+			<div><img src="image.jpg" class="lojas_em_destaque"></div>
+			<div><img src="image.jpg" class="lojas_em_destaque"></div>
+			<div><img src="image.jpg" class="lojas_em_destaque"></div>
+			<div><img src="image.jpg" class="lojas_em_destaque"></div>
+			<div><img src="image.jpg" class="lojas_em_destaque"></div>
+			<div><img src="image.jpg" class="lojas_em_destaque"></div>
+			<div><img src="image.jpg" class="lojas_em_destaque"></div>
+			<div><img src="image.jpg" class="lojas_em_destaque"></div>
+			<div><img src="image.jpg" class="lojas_em_destaque"></div>
+			<div><img src="image.jpg" class="lojas_em_destaque"></div>
+			<div><img src="image.jpg" class="lojas_em_destaque"></div>
+			<div><img src="image.jpg" class="lojas_em_destaque"></div>
+			<div><img src="image.jpg" class="lojas_em_destaque"></div>
+			<div><img src="image.jpg" class="lojas_em_destaque"></div>
+			<div><img src="image.jpg" class="lojas_em_destaque"></div>
+		</div>
+		
+		<div class="barra_pagamento"></div>
+		
+		
+	<div id="branco">
+		
+	</div>
+	
+	</body>
+</html>
